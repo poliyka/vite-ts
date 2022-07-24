35865d74b9ad02699ead971d4741fc263f4c1b7d
poliyka <jkny3586@gmail.com>
2022-07-24 13:12:44 +0800

    feat(build): setup env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+console.log(process.env.NODE_ENV);
+
+export default defineConfig(({ mode }) => {
+  const defaultConfig = {
+    plugins: [vue()],
+    server: {
+      host: "0.0.0.0",
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    base: "/smart-dev/",
+  };
+
+  if (mode == "production") {
+    defaultConfig.base = "/smart-prod/";
+  } else {
+    defaultConfig.base = "/smart-dev/";
   }
-})
+
+  return defaultConfig;
+});
