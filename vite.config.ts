9f577b24e4cb46cb2d781149ba83606f4a0a0c01
poliyka <jkny3586@gmail.com>
2022-07-24 16:14:23 +0800

    feat(build): setup dev, prod(80), docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,15 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-console.log(process.env.NODE_ENV);
 
-export default defineConfig(({ mode }) => {
-  const defaultConfig = {
-    plugins: [vue()],
-    server: {
-      host: "0.0.0.0",
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-  };
-
-  return defaultConfig;
+  },
 });
